1734672855bdabc091f1cc92cf669c277f31248d
unknown <arthur.lolico@gmail.com>
2022-06-14 09:18:08 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/header.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
